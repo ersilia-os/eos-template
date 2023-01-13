1dbd0a6db2371698424dba39d29f6e2b84891c06
gemmaturon <gemma@ersilia.io>
2023-01-13 11:04:36 +0100

    Update main.py
    Temporarily remove .joblib file as git-lfs does not work in git templates

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -14,14 +14,17 @@ output_file = sys.argv[2]
 root = os.path.dirname(os.path.abspath(__file__))
 
 # checkpoints directory
-checkpoints_dir = os.path.abspath(os.path.join(root, "..", "..", "checkpoints"))
+#checkpoints_dir = os.path.abspath(os.path.join(root, "..", "..", "checkpoints"))
 
 # read checkpoints (here, simply an integer number: 42)
-ckpt = joblib.load(os.path.join(checkpoints_dir, "checkpoints.joblib"))
+#ckpt = joblib.load(os.path.join(checkpoints_dir, "checkpoints.joblib"))
 
 # model to be run (here, calculate the Molecular Weight and add ckpt (42) to it)
-def my_model(smiles_list, ckpt):
-    return [MolWt(Chem.MolFromSmiles(smi))+ckpt for smi in smiles_list]
+#def my_model(smiles_list, ckpt):
+    #return [MolWt(Chem.MolFromSmiles(smi))+ckpt for smi in smiles_list]
+
+def my_model(smiles_list):
+    return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
     
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -30,7 +33,8 @@ with open(input_file, "r") as f:
     smiles_list = [r[0] for r in reader]
     
 # run model
-outputs = my_model(smiles_list, ckpt)
+#outputs = my_model(smiles_list, ckpt)
+outputs = my_model(smiles_list)
 
 # write output in a .csv file
 with open(output_file, "w") as f:
