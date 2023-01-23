38069e63a2a5cefae056b93f6e9c248e87d5fd41
miquelduranfrigola <miquelduranfrigola@gmail.com>
2023-01-23 13:03:54 +0100

    skip contributor

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,7 +1,6 @@
 # imports
 import os
 import csv
-import joblib
 import sys
 from rdkit import Chem
 from rdkit.Chem.Descriptors import MolWt
@@ -13,17 +12,7 @@ output_file = sys.argv[2]
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
 
-# checkpoints directory
-# checkpoints_dir = os.path.abspath(os.path.join(root, "..", "..", "checkpoints"))
-
-# read checkpoints (here, simply an integer number: 42)
-# ckpt = joblib.load(os.path.join(checkpoints_dir, "checkpoints.joblib"))
-
-# model to be run (here, calculate the Molecular Weight and add ckpt (42) to it)
-# def my_model(smiles_list, ckpt):
-# return [MolWt(Chem.MolFromSmiles(smi))+ckpt for smi in smiles_list]
-
-
+# my model
 def my_model(smiles_list):
     return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
 
@@ -35,7 +24,6 @@ with open(input_file, "r") as f:
     smiles_list = [r[0] for r in reader]
 
 # run model
-# outputs = my_model(smiles_list, ckpt)
 outputs = my_model(smiles_list)
 
 # write output in a .csv file
