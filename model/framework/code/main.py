676d9c4db16aaafdb9075c2389cfd5669fc0dfbc
Marina18 <marina@ersilia.io>
2026-03-13 10:30:06 +0100

    update template to use ersilia-pack-utils for I/O operations

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,8 +2,10 @@
 import os
 import csv
 import sys
+import numpy as np
 from rdkit import Chem
 from rdkit.Chem.Descriptors import MolWt
+from ersilia_pack_utils.core import read_smiles, write_out
 
 # parse arguments
 input_file = sys.argv[1]
@@ -18,10 +20,7 @@ def my_model(smiles_list):
 
 
 # read SMILES from .csv file, assuming one column with header
-with open(input_file, "r") as f:
-    reader = csv.reader(f)
-    next(reader)  # skip header
-    smiles_list = [r[0] for r in reader]
+_, smiles_list = read_smiles(input_file)
 
 # run model
 outputs = my_model(smiles_list)
@@ -31,9 +30,8 @@ input_len = len(smiles_list)
 output_len = len(outputs)
 assert input_len == output_len
 
+num_dims = outputs.shape[1]
+header = [f"feat_{str(i).zfill(3)}" for i in range(num_dims)]
+
 # write output in a .csv file
-with open(output_file, "w") as f:
-    writer = csv.writer(f)
-    writer.writerow(["value"])  # header
-    for o in outputs:
-        writer.writerow([o])
+write_out(outputs, header, output_file, np.float32)
